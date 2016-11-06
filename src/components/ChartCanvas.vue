60d020d12b6c803388530f66b2befcf05c924813
dorkpon <ynwangpeng@gmail.com>
2016-11-06 14:43:31 +0800

    bar => liner

--- src/components/ChartCanvas.vue
+++ src/components/ChartCanvas.vue
@@ -12,68 +12,32 @@
 <script>
 import Chart from 'chart.js'
 
-const chartData = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [{
-    fillColor: 'rgba(220,220,220,0.5)',
-    strokeColor: 'rgba(220,220,220,1)',
-    pointColor: 'rgba(220,220,220,1)',
-    pointStrokeColor: '#fff',
-    data: [65, 59, 90, 81, 56, 55, 40]
-  }, {
-    fillColor: 'rgba(151,187,205,0.5)',
-    strokeColor: 'rgba(151,187,205,1)',
-    pointColor: 'rgba(151,187,205,1)',
-    pointStrokeColor: '#fff',
-    data: [28, 48, 40, 19, 96, 27, 100]
-  }]
-}
-
 export default {
   data () {
     return {}
   },
 
   mounted () {
-    console.log(this.$el)
-    console.log(chartData)
     /* eslint-disable no-new */
     new Chart(this.$el, {
-      type: 'bar',
+      type: 'line',
       data: {
         labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         datasets: [{
           label: 'outer door',
-          data: [30, 19, 23, 56, 12, 39, 23],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(220, 220, 220, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(220, 220, 220, 1)'
-          ],
-          borderWidth: 1
+          fill: false,
+          backgroundColor: 'rgba(255, 99, 132, 0.8)',
+          borderColor: 'rgba(255, 99, 132, 0.8)',
+          pointBackgroundColor: '#fff',
+          data: [65, 59, 90, 81, 56, 55, 40]
+        }, {
+          label: 'inner door',
+          fill: false,
+          backgroundColor: 'rgba(75, 192, 192, 0.8)',
+          borderColor: 'rgba(75, 192, 192, 0.8)',
+          pointBackgroundColor: '#fff',
+          data: [28, 18, 40, 19, 23, 21, 20]
         }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
       }
     })
   }
